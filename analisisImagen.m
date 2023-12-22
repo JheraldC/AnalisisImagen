a32ee4737dc9802fc99a3095f900c0e306860515
JHERALD HUREN CACERES APAZA <93649530+JheraldC@users.noreply.github.com>
2023-12-22 12:52:20 -0500

    Update analisisImagen.m

--- analisisImagen.m
+++ analisisImagen.m
@@ -66,34 +66,58 @@ disp('Información sobre el uso de memoria:');
 disp(['Memoria total: ' num2str(mem_info.MemAvailableAllArrays / (1024^2)) ' MB']);
 disp(['Memoria utilizada: ' num2str(mem_info.MemUsedMATLAB / (1024^2)) ' MB']);
 
-% Calcular la métrica de la relación calidad/tamaño
-metrica_relacion = calidades_niqe .* tamanos_archivos;
+% Normalizar NIQE (menor es mejor)
+max_niqe = max(calidades_niqe);
+min_niqe = min(calidades_niqe);
+niqe_normalizado = (calidades_niqe - min_niqe) / (max_niqe - min_niqe);
 
-% Encontrar la imagen con la mejor métrica
-[mejor_metrica, indice_mejor] = max(metrica_relacion);
+% Normalizar Tamaño del Archivo (menor es mejor)
+max_tamano = max(tamanos_archivos);
+min_tamano = min(tamanos_archivos);
+tamano_normalizado = (tamanos_archivos - min_tamano) / (max_tamano - min_tamano);
+
+% Definir pesos (ajustar estos según la importancia relativa)
+peso_niqe = 0.7; % Mayor peso a la calidad
+peso_tamano = 0.3; % Menor peso al tamaño
+
+% Calcular puntuación compuesta
+puntuacion_compuesta = peso_niqe * niqe_normalizado + peso_tamano * tamano_normalizado;
+
+% Encontrar la imagen con la puntuación compuesta más baja
+[~, indice_mejor] = min(puntuacion_compuesta);
 
 % Detener el temporizador y mostrar el tiempo de ejecución
 tiempo_ejecucion = toc;
 disp(['Tiempo de ejecución: ' num2str(tiempo_ejecucion) ' segundos']);
 
 % Mostrar la información de la mejor imagen
-disp(['Mejor imagen:']);
+disp(['Mejor imagen según puntuación compuesta:']);
 disp(['  Ruta: ' nombres_imagenes{indice_mejor}]);
 disp(['  Tamaño del archivo: ' num2str(tamanos_archivos(indice_mejor)) ' KB']);
 disp(['  Calidad (NIQE): ' num2str(calidades_niqe(indice_mejor))]);
-disp(['  Métrica de Relación: ' num2str(mejor_metrica)]);
+disp(['  Puntuación Compuesta: ' num2str(puntuacion_compuesta(indice_mejor))]);
 
 % Guardar la información en un archivo de texto
 nombre_archivo = 'resultados.txt';
 fid = fopen(nombre_archivo, 'w');
+
 fprintf(fid, 'Información sobre el uso de memoria:\n');
 fprintf(fid, 'Memoria total: %.4f MB\n', mem_info.MemAvailableAllArrays / (1024^2));
 fprintf(fid, 'Memoria utilizada: %.4f MB\n', mem_info.MemUsedMATLAB / (1024^2));
 fprintf(fid, 'Tiempo de ejecución: %.4f segundos\n\n', tiempo_ejecucion);
 
-fprintf(fid, 'Mejor imagen:\n');
+fprintf(fid, 'Resultados de Análisis de Imágenes:\n');
+for i = 1:numel(nombres_imagenes)
+    fprintf(fid, 'Imagen %d: %s\n', i, nombres_imagenes{i});
+    fprintf(fid, '  Tamaño del archivo: %.4f KB\n', tamanos_archivos(i));
+    fprintf(fid, '  Calidad (NIQE): %.4f\n', calidades_niqe(i));
+    fprintf(fid, '  Puntuación Compuesta: %.4f\n\n', puntuacion_compuesta(i));
+end
+
+fprintf(fid, 'Mejor imagen según puntuación compuesta:\n');
 fprintf(fid, '  Ruta: %s\n', nombres_imagenes{indice_mejor});
 fprintf(fid, '  Tamaño del archivo: %.4f KB\n', tamanos_archivos(indice_mejor));
 fprintf(fid, '  Calidad (NIQE): %.4f\n', calidades_niqe(indice_mejor));
+fprintf(fid, '  Puntuación Compuesta: %.4f\n', puntuacion_compuesta(indice_mejor));
 
 fclose(fid);
